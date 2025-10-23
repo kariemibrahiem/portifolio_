252835d027890ede73cf55edd56d333be81091c9
kariemibrhaiem <kariemibrhaiem110@gmail.com>
2025-10-24 01:35:35 +0300

    update_back_end

--- src/componant/3-main/card.js
+++ src/componant/3-main/card.js
@@ -8,26 +8,30 @@ import ic3 from "../images/picture ,icons/phone.gif";
 import p1 from "./projs/chair.png";
 import Lottie from "react-lottie";
 
-const Cards = (props) =>{
-   
-    return(
+const Cards = (props) => {
+
+    return (
         <div>
-            <Card className="card_sec"> 
-            <CardImg src={props.link}/>
+            <Card className="card_sec">
+                <CardImg
+                    src={props.link}
+                    style={{ maxHeight: '100px', objectFit: 'cover', width: '100%' }}
+                />
+
                 <Card.Body className="crd">
                     <h2 className="card_title">{props.title}</h2>
-                    <p className="cat">by {props.cat}</p>
+                    <p className="cat">by kariem ibrahiem</p>
                     <Card.Text>
                         {props.text}
                     </Card.Text>
-                    <small>to see the full deploy of project click at take a look</small>
+                    <small> click to take a look </small>
                     <div className="card_btn">
                         <div>
-                        <a href="https://github.com/kariemibrahiem"> <img className="icon" src={ic1}/></a>
-                        <a href="https://www.linkedin.com/in/kariem-ibrahiem-903a0b2a7?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app"><img className="icon" src={ic2}/></a>
+                            <a target="_blank" href="https://github.com/kariemibrahiem"> <img className="icon" src={ic1} /></a>
+                            <a target="_blank" href="https://www.linkedin.com/in/kariem-ibrahiem-903a0b2a7?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app"><img className="icon" src={ic2} /></a>
                         </div>
                         <div>
-                            <button className="btn"><a  href={props.more}>take a look</a></button>
+                            <button className="btn"><a target="_blank" href={props.more}>take a look</a></button>
                         </div>
                     </div>
                 </Card.Body>
--- src/componant/3-main/main.js
+++ src/componant/3-main/main.js
@@ -14,46 +14,52 @@ import p8 from "./projs/xo.png";
 import f1 from "./projs/f1_shose.png";
 import f2 from "./projs/f2_cof.png";
 import l1 from "./projs/laravel_1.png";
+import travel_link from "./projs/travel.png";
+import mawhebtac_link from "./projs/mawhebtac.png";
+import emlaazon_link from "./projs/emlaazon.png";
+import clinizone_link from "./projs/clinizone.png";
 import l2 from "./projs/l2_htls.png";
 import l3 from "./projs/l3_task.png";
 
-const mycard= [
-   {title :" bank invoices " ,text : "invoices full system to a banks" ,  cat:"back" , link:l1 , more:"https://github.com/kariemibrahiem/laravel_invoices/"},
-   {title :" hotels task " ,text : "hotel partially system to a make relation tasks" ,  cat:"back" , link:l2 , more:"https://github.com/kariemibrahiem/laravel_hotels/"},
-   {title :"  task_queue_manager " ,text : " task as task manager to manage the load on the server using job/queue" ,  cat:"back" , link:l3 , more:"https://github.com/kariemibrahiem/laravel_tasks_management-system/tree/master"},
-   {title :"shose" ,text : "small project for sport products with my ui/ux designing" ,  cat:"front" , link: f1 , more:"https://github.com/kariemibrahiem/react_shoes_com"},
-   {title :"clinic" ,text : "design for a clinic  and my first project with many mistakes" ,  cat:"front" , link: p3 , more:"https://github.com/kariemibrahiem/react_clinec"},
-   {title :"coffee corner" ,text : "design for a coffee with responsive design and perfect animations" ,  cat:"front" , link: f2 , more:"https://github.com/kariemibrahiem/react_final_coffe_corner"},
-   {title :"furniture" ,text : "small project for a furniture company with some adits from me " ,  cat:"react" , link: p1 , more:"https://github.com/kariemibrahiem/react_furniture"},
-   {title :"company" ,text : "design for a company with a color changable buttons js functional" ,  cat:"front" , link: p4 , more:"https://github.com/kariemibrahiem/react_furniture"},
-   {title :"cruds" ,text : "project based on the functions of javascript" ,  cat:"front" , link: p5 , more:"https://github.com/kariemibrahiem/cr"},
-   {title :"de'monetor" ,text : "small project used for the lectures of the faculty " ,  cat:"react" , link: p6 , more:"https://github.com/kariemibrahiem/monetor_react"},
-   {title :"moon" ,text : "login website with js functional to display a writer text" ,  cat:"front" , link: p7 , more:"https://github.com/kariemibrahiem/react_moon"},
-   {title :"XO" ,text : "simple xo game with js functional in very simple design" ,  cat:"front" , link: p8 , more:"https://github.com/kariemibrahiem/react_xo_game"}
-   
-
+const mycard = [
+    { title: "travel club", text: "full hostel , travel porject for hotels , transport  and entertainment companies ;  with admin , vendor dashboard and mobile app ", cat: "back", link: travel_link, more: "https://trvlgate.com/en/admin" },
+    { title: "mawhebtac", text: "full for Talent discovery and casting platform for entertainment industry ; with admin , vendor dashboard and mobile app ", cat: "back", link: mawhebtac_link, more: "https://mawhebtac.com/admin/login" },
+    { title: "emlaazon", text: "full LMS project for manage transactions between teacher and student  ; with admin dashboard and mobile app ", cat: "back", link: emlaazon_link, more: "https://platform2.egymoe.com/public/admin" },
+    { title: "clinizone", text: "full back/front end project integrated with odoo external api handle more than 50k record; with admin dashboard  ", cat: "back", link: clinizone_link, more: "https://crm.clinizone.net/dashboard/medical-audit" },
+    { title: " bank invoices ", text: "invoices full system to a banks", cat: "back", link: l1, more: "https://github.com/kariemibrahiem/laravel_invoices/" },
+    { title: " hotels task ", text: "hotel partially system to a make relation tasks", cat: "back", link: l2, more: "https://github.com/kariemibrahiem/laravel_hotels/" },
+    { title: "  task_queue_manager ", text: " task as task manager to manage the load on the server using job/queue", cat: "back", link: l3, more: "https://github.com/kariemibrahiem/laravel_tasks_management-system/tree/master" },
+    { title: "shose", text: "small project for sport products with my ui/ux designing", cat: "front", link: f1, more: "https://github.com/kariemibrahiem/react_shoes_com" },
+    { title: "clinic", text: "design for a clinic  and my first project with many mistakes", cat: "front", link: p3, more: "https://github.com/kariemibrahiem/react_clinec" },
+    { title: "coffee corner", text: "design for a coffee with responsive design and perfect animations", cat: "front", link: f2, more: "https://github.com/kariemibrahiem/react_final_coffe_corner" },
+    { title: "furniture", text: "small project for a furniture company with some adits from me ", cat: "react", link: p1, more: "https://github.com/kariemibrahiem/react_furniture" },
+    { title: "company", text: "design for a company with a color changable buttons js functional", cat: "front", link: p4, more: "https://github.com/kariemibrahiem/react_furniture" },
+    { title: "cruds", text: "project based on the functions of javascript", cat: "front", link: p5, more: "https://github.com/kariemibrahiem/cr" },
+    { title: "de'monetor", text: "small project used for the lectures of the faculty ", cat: "react", link: p6, more: "https://github.com/kariemibrahiem/monetor_react" },
+    { title: "moon", text: "login website with js functional to display a writer text", cat: "front", link: p7, more: "https://github.com/kariemibrahiem/react_moon" },
+    { title: "XO", text: "simple xo game with js functional in very simple design", cat: "front", link: p8, more: "https://github.com/kariemibrahiem/react_xo_game" }
 ];
 // const arr = [{title : "main"}]
-const Main = () =>{
+const Main = () => {
     const handel = (par) => {
         const newArr = mycard.filter((elements) => {
             return elements.cat == par;
         });
         setarr(newArr);
     }
-    
-    var [active , setactive] = useState(0); {/*the activate buttons */}
 
-    const [arr , setarr] = useState(mycard);
-    return(
+    var [active, setactive] = useState(0); {/*the activate buttons */ }
+
+    const [arr, setarr] = useState(mycard);
+    return (
         <div className="container main_sec">
             <h1 className="main_pj">some projects </h1>
             <p className="main_pj">there are more projects in githup  </p>
             <div className="main_con">
                 <div className=" btn_div ">
-                    <button onClick={() => {setactive(0); setarr(mycard)}} className= {active == 0 ? "btn btn-outline-success m-1 active" : "btn btn-outline-success m-1 "} >all projects</button>
-                    <button onClick={() => {setactive(1); handel("front") ;}} className= {active == 1 ? "btn btn-outline-success m-1 active" : "btn btn-outline-success m-1 "} >front-end</button>
-                    <button onClick={() => {setactive(2); handel("back") ;}} className= {active == 2 ? "btn btn-outline-success m-1 active" : "btn btn-outline-success m-1 "} >back-ens</button>
+                    <button onClick={() => { setactive(0); setarr(mycard) }} className={active == 0 ? "btn btn-outline-success m-1 active" : "btn btn-outline-success m-1 "} >all projects</button>
+                    <button onClick={() => { setactive(2); handel("back"); }} className={active == 2 ? "btn btn-outline-success m-1 active" : "btn btn-outline-success m-1 "} >back-ens</button>
+                    <button onClick={() => { setactive(1); handel("front"); }} className={active == 1 ? "btn btn-outline-success m-1 active" : "btn btn-outline-success m-1 "} >front-end</button>
                 </div>
                 <div className=" cards_div ">
                     {/* {active == 0 ? <div className=""><Cards title="page design 1" picture={p1} /></div> : null}
@@ -61,10 +67,10 @@ const Main = () =>{
                     {active == 2 ? <div className=""><Cards title="page design 3" picture={p3} /></div> : null}
                     {active == 3 ? <div className=""><Cards title="page design 4" picture={p4} /></div> : null}
                     {active == 4 ? <div className=""><Cards title="page design 5" picture={p5} /></div> : null}  */}
-                    {arr.map((item)=>{
-                        return(
+                    {arr.map((item) => {
+                        return (
                             <div>
-                                <Cards title={item.title} cat={item.cat} link={item.link} text={item.text} more={item.more}/>
+                                <Cards title={item.title} cat={item.cat} link={item.link} text={item.text} more={item.more} />
                             </div>
                         );
                     })}
